f455e430bf4e1a95e3468101c7fa57325bd727cc
Princehingrajiya <hingrajiyaprinceliladharbhai@gmail.com>
2023-01-02 18:20:42 +0530

    working on edit todo, integrated material ui\

--- src/Todolist.js
+++ src/Todolist.js
@@ -8,6 +8,8 @@ import { Checkbox } from "@mui/material";
 import Container from "@mui/material/Container";
 import DeleteIcon from "@mui/icons-material/Delete";
 // import TOlist from "./TOlist";
+import Box from "@mui/material/Box";
+import SaveIcon from "@mui/icons-material/Save";
 
 const Todolist = () => {
   const [initial, setinitial] = useState("");
@@ -24,7 +26,10 @@ const Todolist = () => {
       return [...prevalue, initial];
     });
 
-    setinitial(" ");
+    // console.log(newitem);
+    // console.log(initial);
+
+    setinitial("");
   };
 
   const deleteItem = (id) => {
@@ -37,14 +42,26 @@ const Todolist = () => {
     });
   };
 
+  // updating...........................
+
   const handledit = (id) => {
-    setinitial(newitem[id]);
+    // newitem.splice(editindex, 1, initial);
+    // setnewitem("");
+    seteditindex(newitem[id]);
+    console.log(newitem[id]);
+
+    // newitem.splice(editindex, 1, newitem);
+    // seteditindex(initial[id]);
+    // setnewitem(...newitem[id]);
+
     setshow(true);
     seteditindex(id);
   };
 
   const update = () => {
+    // array splice method  =  add editiindex , 1 remove , adding values like initial.....
     newitem.splice(editindex, 1, initial);
+    // newitem.splice(editindex, 1, newitem);
     setnewitem([...newitem]);
     setshow(false);
     setinitial("");
@@ -52,24 +69,24 @@ const Todolist = () => {
 
   return (
     <>
-      <Container maxWidth="sm" className="contai">
-        <div className="frm">
-          <div className="fom">
-            <div className="header">
-              <Grid className="hed">TODO APP</Grid>
-            </div>
-            <div className="intp">
-              <Input
-                type="text"
-                placeholder="Enter your Items"
-                className="int"
-                value={initial}
-                onChange={handlist}
-              />
-            </div>
+      <Box>
+        <Container maxWidth="sm" className="contai">
+          <div className="frm">
+            <div className="fom">
+              <div className="header">
+                <Grid className="hed">TODO APP</Grid>
+              </div>
+              <div className="intp">
+                <Input
+                  type="text"
+                  placeholder="Enter your Items"
+                  className="int"
+                  value={initial}
+                  onChange={handlist}
+                />
+              </div>
 
-            <div className="butt">
-              {!show ? (
+              <div className="butt">
                 <Button
                   variant="contained"
                   className="btn"
@@ -77,40 +94,85 @@ const Todolist = () => {
                 >
                   ADD
                 </Button>
-              ) : (
-                <Button variant="contained" className="btn" onClick={update}>
-                  Save
-                </Button>
-              )}
-            </div>
 
-            <div className="item">
-              <div className="ite">
-                {newitem.map((val, id) => {
-                  return (
-                    <>
-                      <Container maxWidth="sm">
-                        <Paper className="todlist">
-                          <Checkbox></Checkbox>
-                          <div className="list">{val}</div>
-                          <EditIcon
-                            className="edit"
-                            onClick={() => handledit(id)}
-                          ></EditIcon>
-                          <DeleteIcon
-                            className="clear"
-                            onClick={() => deleteItem(id)}
-                          ></DeleteIcon>
-                        </Paper>
-                      </Container>
-                    </>
-                  );
-                })}
+                {!show ? (
+                  <textarea
+                    type="text"
+                    placeholder="edit item"
+                    value={handledit}
+                  ></textarea>
+                ) : (
+                  ""
+                )}
+
+                {/* {!show ? (
+                  <Button
+                    variant="contained"
+                    className="btn"
+                    onClick={clickevent}
+                  >
+                    Add
+                  </Button>
+                ) : (
+                  <Button variant="contained" className="btn" onClick={update}>
+                    Save
+                  </Button>
+                )} */}
+
+                {/* {!show ? (
+                  <SaveIcon className="btn" onClick={update}></SaveIcon>
+                ) : (
+                  <EditIcon
+                    className="edit"
+                    onClick={(id) => handledit(id)}
+                  ></EditIcon>
+                )} */}
+
+                {/* {!show ? (
+                  <SaveIcon type="text" onChange={update}></SaveIcon>
+                ) : (
+                  <Button
+                    variant="contained"
+                    className="btn"
+                    onClick={clickevent}
+                  >
+                    ADD
+                  </Button>
+                )} */}
+              </div>
+
+              <div className="item">
+                <div className="ite">
+                  {newitem.map((val, id) => {
+                    return (
+                      <>
+                        <Container maxWidth="sm">
+                          <Paper className="todlist">
+                            <Checkbox></Checkbox>
+                            <div className="list">{val}</div>
+                            <EditIcon
+                              className="edit"
+                              onClick={() => handledit(id)}
+                            >
+                              {/* {
+                                   !show ? (<button onClick={clickevent}></button>):( < />)
+                                 } */}
+                            </EditIcon>
+                            <DeleteIcon
+                              className="clear"
+                              onClick={() => deleteItem(id)}
+                            ></DeleteIcon>
+                          </Paper>
+                        </Container>
+                      </>
+                    );
+                  })}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </Box>
     </>
   );
 };
